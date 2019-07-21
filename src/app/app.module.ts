9d657701c5a49c34c1fc9bc79fda97b9db9171b6
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-07-21 17:18:22 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmailComponent } from './email/email.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyA7Vrt7_W89bwKGPabK3eahBiEnBRHvIV8',
+  authDomain: 'nimedco-pharmacy.firebaseapp.com',
+  databaseURL: 'https://nimedco-pharmacy.firebaseio.com',
+  storageBucket: 'nimedco-pharmacy.appspot.com',
+  messagingSenderId: '186334638758'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    EmailComponent,
+    MembersComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+
+  apiKey: "AIzaSyA7Vrt7_W89bwKGPabK3eahBiEnBRHvIV8",
+    authDomain: "nimedco-pharmacy.firebaseapp.com",
+    databaseURL: "https://nimedco-pharmacy.firebaseio.com",
+    projectId: "nimedco-pharmacy",
+    storageBucket: "nimedco-pharmacy.appspot.com",
+    messagingSenderId: "186334638758",
+    appId: "1:186334638758:web:56a1003424b694b7"
 };
